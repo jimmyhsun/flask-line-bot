ae66d83b12aad10d2c273ef6a1e195443c073ffc
jimmyhsun <97586032+jimmyhsun@users.noreply.github.com>
2022-02-25 14:00:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -744,7 +744,8 @@ def submit():
         j = request.form.get('j')
         k = request.form.get('k')
         l = request.form.get('l')
-        c=int(c);d=int(d);e=int(e);f=int(f);g=int(g);h=int(h);i=int(i);j=int(j);k=int(k);l=int(l)
+        m = request.form.get('m')
+        c=int(c);d=int(d);e=int(e);f=int(f);g=int(g);h=int(h);i=int(i);j=int(j);k=int(k);l=int(l);m = int(m)
 
         money = c*10 + d*10 +e*20+f*20+g*10+h*30+i*20+j*5+k*10+l*20
         calorie = c*210+d*8+e*194+f*192+g*154+h*202+i*199+j*180+k*226+l*221
